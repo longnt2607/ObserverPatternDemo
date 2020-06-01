4eab7d16df696098ee5579e78bd4485273d853e7
longnt2607 <ntlong12c1ncthanh@gmail.com>
2020-06-01 21:11:11 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/nguyentruonglong/observerpatterndemo/MainActivity.java
@@ -0,0 +1,90 @@
+package vn.edu.ntu.nguyentruonglong.observerpatterndemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Calendar;
+
+import vn.edu.ntu.nguyentruonglong.util.MyDateDialog;
+import vn.edu.ntu.nguyentruonglong.util.MyTimeDialog;
+
+public class MainActivity extends AppCompatActivity implements MyDateDialog.OnMyDateChangeListener, MyTimeDialog.OnMyTimeChangeListener {
+
+    EditText dateCongTac;
+    EditText timeCongTac;
+    ImageView imvDate;
+    ImageView imvTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addViews();
+        setViews();
+    }
+
+    private void addViews () {
+        dateCongTac = findViewById(R.id.dateCongTac);
+        dateCongTac.setEnabled(false);
+        imvDate = findViewById(R.id.imvDate);
+
+        timeCongTac = findViewById(R.id.timeCongTac);
+        timeCongTac.setEnabled(false);
+        imvTime = findViewById(R.id.imvTime);
+    }
+
+    private void setViews() {
+        imvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDateDialog myDateDialog = new MyDateDialog(MainActivity.this, Calendar.getInstance(), MainActivity.this);
+                myDateDialog.showDateDialog();
+            }
+        });
+
+        imvTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyTimeDialog myTimeDialog = new MyTimeDialog(MainActivity.this, Calendar.getInstance(), MainActivity.this);
+                myTimeDialog.showTimeDialog(timeCongTac);
+            }
+        });
+    }
+    /*private void setViews () {
+        imvCongtac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDateDialog myDateDialog = new MyDateDialog(MainActivity.this, Calendar.getInstance(), MainActivity.this);
+                myDateDialog.showDateDialog();
+            }
+        });
+    }*/
+
+    @Override
+    public void dateUpdate(Calendar newDate) {
+        StringBuilder stringBuilder  = new StringBuilder();
+        stringBuilder.append(newDate.get(newDate.DAY_OF_MONTH))
+                .append("/")
+                .append(newDate.get(newDate.MONTH) + 1)
+                .append("/")
+                .append(newDate.get(newDate.YEAR));
+        dateCongTac.setText(stringBuilder.toString());
+    }
+
+    @Override
+    public void timeUpdate(Calendar newTime) {
+        /*int hourOfDay = newTime.get(newTime.HOUR_OF_DAY);
+        int minute = newTime.get(newTime.MINUTE);
+        String amPm;
+        if (hourOfDay >= 12) {
+            amPm = "PM";
+        } else {
+            amPm = "AM";
+        }
+        timeCongTac.setText(String.format("%02d:%02d", hourOfDay, minute) + amPm);*/
+    }
+}
---
+++ app/src/main/java/vn/edu/ntu/nguyentruonglong/util/MyTimeDialog.java
@@ -0,0 +1,44 @@
+package vn.edu.ntu.nguyentruonglong.util;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.time.Clock;
+import java.util.Calendar;
+
+public class MyTimeDialog {
+    Context context;
+    Calendar calendar;
+    OnMyTimeChangeListener onMyTimeChangeListener;
+    String amPm;
+
+    public MyTimeDialog(Context context, Calendar calendar, OnMyTimeChangeListener onMyTimeChangeListener) {
+        this.context = context;
+        this.calendar = calendar;
+        this.onMyTimeChangeListener = onMyTimeChangeListener;
+    }
+
+    public void showTimeDialog (final EditText text) {
+        TimePickerDialog.OnTimeSetListener listener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                if (hourOfDay >= 12) {
+                    amPm = "PM";
+                } else {
+                    amPm = "AM";
+                }
+                text.setText(String.format("%02d:%02d ", hourOfDay, minute) + amPm);
+                if (onMyTimeChangeListener != null) onMyTimeChangeListener.timeUpdate(calendar);
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(context, listener, calendar.get(calendar.HOUR_OF_DAY), calendar.get(calendar.MINUTE), false);
+        timePickerDialog.show();
+    }
+
+    public static interface OnMyTimeChangeListener {
+        public void timeUpdate(Calendar newTime);
+    }
+}
